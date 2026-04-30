fbd96ad619a0e2adea6fe0e62e7017e0ab36b622
Rita Agarwala <rita.agarwala@salesforce.com>
2026-04-30 20:58:52 +0530

    feat!: restructure function-specific flags for CLI
    BREAKING CHANGE: Command signatures changed for function init, deploy, and run
    
    @W-22278901

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git add -A && git commit -m "feat!: restructure function-specific flags for CLI
-
-- Change --used-in-feature to --use-in-feature
-
-BREAKING CHANGE: Command signatures changed for function init, deploy, and run
-
-@W-22278901"
